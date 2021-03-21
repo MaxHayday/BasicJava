590fa21090932bbc72ec3804291dad9fd10653c0
MaxHayday <filipenko.91@mail.ru>
2021-03-21 02:07:13 +0200

    Method which count quantity of fridays 13 from between years EVGENIY EXAMPLE

--- src/main/java/com/maxhayday/hw/hw03/friday13/FridayCalculator.java
+++ src/main/java/com/maxhayday/hw/hw03/friday13/FridayCalculator.java
@@ -2,6 +2,11 @@ package com.maxhayday.hw.hw03.friday13;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class FridayCalculator {
 
@@ -11,5 +16,18 @@ public class FridayCalculator {
         return (int) fromDate.datesUntil(toDate)
                 .filter(f -> f.getDayOfWeek() == DayOfWeek.FRIDAY && f.getDayOfMonth() == 13)
                 .count();
+    }
+
+    //example by Evgeniy
+    public static void printFriday13(int start, int end) {
+        LocalDate startDate = LocalDate.of(start, 1, 13);
+        LocalDate endDate = LocalDate.of(end, 1, 14);
+        Stream.iterate(startDate, localDate -> localDate.plusMonths(1))
+                .limit(ChronoUnit.MONTHS.between(startDate, endDate))
+                .filter(localDate -> localDate.getDayOfWeek() == DayOfWeek.FRIDAY)
+                .collect(groupingBy(LocalDate::getYear, counting()))
+                .entrySet().stream()
+                .sorted((t, t1) -> (int) (t1.getValue() - t.getValue()))
+                .forEach(System.out::println);
     }
 }
--- src/main/java/com/maxhayday/hw/hw03/friday13/Main.java
+++ src/main/java/com/maxhayday/hw/hw03/friday13/Main.java
@@ -3,6 +3,7 @@ package com.maxhayday.hw.hw03.friday13;
 public class Main {
     public static void main(String[] args) {
         int countOfAllFridaysOfYears = FridayCalculator.getCountOfAllFridaysOfYears(2018, 2020);
-        System.out.println(countOfAllFridaysOfYears);
+       // System.out.println(countOfAllFridaysOfYears);
+        FridayCalculator.printFriday13(1900,1920);
     }
 }
